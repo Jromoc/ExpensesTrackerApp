2d75d16b728fe8a24c588531c35838e73c507d3b
José Romo <jose.e.romo@hotmail.com>
2024-08-17 18:09:07 -0600

    Fecha main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Inicializar la tabla si no existe en session_state
 if "gasto" not in st.session_state:
-    st.session_state.gasto = pd.DataFrame(columns=["Descripción", "Categoría", "Monto"])
+    st.session_state.gasto = pd.DataFrame(columns=["Descripción", "Categoría", "Monto", "Fecha"])
 
  # Formulario para ingresar gastos
 st.header("Registro de Gastos")
@@ -11,11 +11,12 @@ with st.form("registro_form"):
     descripcion = st.text_input("Descripción del gasto")
     categoria = st.selectbox("Categoría", ["Comida", "Transporte", "Entretenimiento", "Otros"])
     monto = st.number_input("Monto", min_value=0.0, format="%.2f")
+    fecha = date.today()
     submitted = st.form_submit_button("Agregar Gasto")
 
     # Agregar nuevo registro a la tabla
     if submitted:
-        nuevo_registro = pd.DataFrame({"Descrpcion": [descripcion], "Categoria": [categoria], "Monto": [monto]})
+        nuevo_registro = pd.DataFrame({"Descrpcion": [descripcion], "Categoria": [categoria], "Monto": [monto], "Fecha": [fecha]})
         st.session_state.gasto = pd.concat([st.session_state.gasto, nuevo_registro], ignore_index=True)
 
 # Mostrar la tabla con los registros acumulados

ecabe5cd63d38c57d89084ed116c00be3f5edf74
José Romo <a01197772@tec.mx>
2024-08-18 03:42:20 -0600

    Actualizacion de pagina de Registro de Gastos

--- main.py
+++ main.py
@@ -46,19 +46,6 @@ def pagina1():
     except Exception as e:
         st.error(f"Error al mostrar la tabla: {e}")
 
-    # Botón para mostrar la gráfica de gastos por categoría
-    if st.button("Mostrar gráfica de gastos por categoría"):
-        if not st.session_state.gasto.empty:
-            try:
-                # Agrupar los gastos por categoría y sumar los montos
-                gastos_por_categoria = st.session_state.gasto.groupby("Categoría")["Monto"].sum()
-
-                # Mostrar la gráfica de barras con st.bar_chart
-                st.bar_chart(gastos_por_categoria)
-            except Exception as e:
-                st.error(f"Error al generar la gráfica: {e}")
-        else:
-            st.write("No hay datos para mostrar en la gráfica.")
 
 def pagina2():
     st.title("Dashboards de Gastos")

248a84202a6c19e22d399de53577c03e3c796da7
José Romo <a01197772@tec.mx>
2024-08-18 03:58:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+from pages import pagina1, pagina2, pagina3
 
+# Sidebar para la navegación
+st.sidebar.title("Navegación")
+opcion = st.sidebar.radio("Ir a", ["Página 1", "Página 2", "Página 3"])
+
+# Cargar la página seleccionada
+if opcion == "Página 1":
+    pagina1.mostrar_pagina()
+elif opcion == "Página 2":
+    pagina2.mostrar_pagina()
+elif opcion == "Página 3":
+    pagina3.mostrar_pagina()
